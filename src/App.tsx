946167b7a8eb4d298026ee79c126044a17806dae
confident2 <mykel.anigboya@yahoo.com>
2024-02-26 07:46:52 -0800

    useRef, useMemo, useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React, {
-  useEffect,
+import {
   useState,
+  useEffect,
   useCallback,
+  useMemo,
+  useRef,
   MouseEvent,
   KeyboardEvent,
 } from "react";
-import List from "./components/List";
 
 interface User {
   id: number;
   username: string;
 }
 
+type fibFunc = (n: number) => number;
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+const myNum: number = 8;
+
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [users, setUsers] = useState<User[] | null>(null);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  console.log(inputRef?.current);
+  console.log(inputRef?.current?.value);
+
   useEffect(() => {
-    console.log("first");
-    console.log("Users:", users);
-    return () => console.log("second");
+    console.log("mounting");
+    console.log("Users: ", users);
+
+    return () => console.log("unmounting");
   }, [users]);
 
   const addTwo = useCallback(
@@ -27,24 +43,41 @@ const App: React.FC = () => {
       e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
     ): void => {
       setCount((prev) => prev + 2);
-      console.log("Event:", e);
+      console.log(e);
     },
     []
   );
 
   const updateUser = () => {
-    setUsers([{ id: 1, username: "user1" }]);
+    if (inputRef?.current && inputRef?.current?.value) {
+      const newUsername = inputRef.current.value;
+      const newUserData: User = {
+        id: users ? users.length + 1 : 1,
+        username: newUsername,
+      };
+      setUsers(users ? [...users, newUserData] : [newUserData]);
+      inputRef.current.value = ""; // Clear input after updating user data
+    }
   };
+  const result = useMemo<number>(() => fib(myNum), []);
 
   return (
     <div className="App">
       <h1>{count}</h1>
-      <button onClick={addTwo}>Add Two</button>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input ref={inputRef} type="text" />
       <button onClick={updateUser}>Update Users</button>
-      <List
-        items={["☕ Coffee", "🌮 Tacos", "💻 Code"]}
-        render={(item: string) => <span className="bold">{item}</span>}
-      />
+      <div>
+        <h2>User Data:</h2>
+        <ul>
+          {users?.map((user) => (
+            <li key={user.id}>
+              ID: {user.id}, Username: {user.username}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
